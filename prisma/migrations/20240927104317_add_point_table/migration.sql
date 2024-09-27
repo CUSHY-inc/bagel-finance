880a5005625abe225af573bf74277f7d95e6b82b
ruemura3 <ruemura3.contact@gmail.com>
2024-09-27 20:43:27 +1000

    chore: add point table

---
+++ prisma/migrations/20240927104317_add_point_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Point` (
+    `userId` VARCHAR(191) NOT NULL,
+    `bagel` BIGINT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Point` ADD CONSTRAINT `Point_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
