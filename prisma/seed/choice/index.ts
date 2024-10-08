a3b43a0d862044c9e0b5a9a9045b2d39cb6f03cd
ruemura3 <ruemura3.contact@gmail.com>
2024-10-08 23:16:43 +1100

    chore: fix seed data

--- prisma/seed/choice/index.ts
+++ prisma/seed/choice/index.ts
@@ -11,7 +11,9 @@ export const choice = () => {
           idx: idx + 1,
           title: `Super index No. ${index * 3 + idx}`,
           image: "/images/seed-index.png",
-          description: `The Super Hyper Ultra Mega Epic Awesome Index No. ${choiceId}`,
+          description: `The Super Hyper Ultra Mega Epic Awesome Index No. ${
+            index * 3 + idx + 1
+          }`,
           result: index < 2 ? ((index * 3 + idx) % 3 === 1 ? 10 : -5) : null,
           isWinner: index < 2 ? (index * 3 + idx) % 3 === 1 : null,
           voteRate: index < 2 ? 33.3 : null,
