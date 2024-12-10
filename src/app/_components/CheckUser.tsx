ce1ca64e7fab79a42b2b1a373b9ad91a0af99a2c
ruemura3 <ruemura3.contact@gmail.com>
2024-12-10 21:22:00 +1100

    refactor: make some refactoring

--- src/app/_components/CheckUser.tsx
+++ src/app/_components/CheckUser.tsx
@@ -3,7 +3,6 @@
 import { fetcher } from "@/lib/swr";
 import { useInitData } from "@telegram-apps/sdk-react";
 import useSWR, { mutate } from "swr";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Loading from "@/app/loading";
 import Error from "@/app/error";
@@ -11,34 +10,31 @@ import { initUser, sendFirstMessage, updateUser } from "./actions";
 import { UserWithLogin } from "@/types/prisma";
 
 export default function CheckUser({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
   const initData = useInitData();
-  const userId = initData?.user?.id;
   const { data, error, isLoading } = useSWR<UserWithLogin>(
-    userId ? `/api/users/${userId}` : null,
+    initData?.user?.id ? `/api/users/${initData?.user?.id}` : null,
     fetcher
   );
 
   useEffect(() => {
     async function process() {
-      if (data === null && initData?.user) {
-        await initUser(initData.user, initData.startParam);
-        await mutate(
-          initData.user?.id ? `/api/users/${initData.user?.id}` : null
-        );
-      } else if (data && initData?.user) {
-        await updateUser(initData.user);
+      if (!initData || !initData.user) {
+        return;
       }
-      if (data && !data.login.sentWelcomeMsg && initData?.user?.id) {
-        await sendFirstMessage(initData.user.id.toString());
-        await mutate(
-          initData.user?.id ? `/api/users/${initData.user?.id}` : null
-        );
+      if (data === null) {
+        await initUser(initData.user, initData.startParam);
+        await mutate(`/api/users/${initData.user.id}`);
+      } else if (data) {
+        await updateUser(initData.user);
+        if (!data.login.sentWelcomeMsg) {
+          await sendFirstMessage(initData.user.id.toString());
+          await mutate(`/api/users/${initData.user.id}`);
+        }
       }
     }
 
     process();
-  }, [data, initData, router]);
+  }, [data, initData]);
 
   if (isLoading || !data) {
     return <Loading />;
